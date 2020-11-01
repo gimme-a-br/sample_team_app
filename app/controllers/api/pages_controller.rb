8a3fae26c7ead9670303ace90603f29f98538e6b
Todd Whitelow <todd@whitelow.tech>
2020-11-01 08:56:20 -0700

    added my name to the json

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,5 +1,5 @@
 class Api::PagesController < ApplicationController
   def index
-    render json: { message: "Hello, my name is Peter!" }
+    render json: { message: "Hello, my name is Peter! And this is Todd" }
   end
 end
